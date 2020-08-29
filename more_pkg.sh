1789e3f1a873c8f5c64bda853ecb6ebe8bcd57f1
Goutham-hub <66170930+Goutham-hub605@users.noreply.github.com>
2020-08-29 00:49:20 -0400

    Add files via upload

---
+++ more_pkg.sh
@@ -0,0 +1,58 @@
+pkg update -y
+pkg upgrade -y
+pkg install tsu -y
+pkg install git -y
+pkg install nano -y
+pkg install python -y
+pkg install python2 -y                 pkg install python3 -y
+pkg install python3-pip -y
+pkg install bash -y
+pkg install curl -y
+pkg install openssl -y
+pkg install ssh -y
+pkg install wget -y
+pkg install php -y
+pkg install ruby -y
+pkg install nmap -y
+pkg install hydra -y
+pkg install wireless-tools -y
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
+pkg install macchanger -y
+pkg install dnsutils -y
+pkg install zip -y
+pkg install dirb -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install tmux -y
+pkg install toilet -y
+pkg install file -y
+pkg install find -y
+pkg install samba -y
+pkg install grep -y
+pip install hashid
+pip install scapy
+pkg install find -y
+pkg install file -y
+pkg install mlocate
+pip install wfuzz
+pip install requests
+pkg install openssh -y
+pkg install openssl -y
+pkg install openssl-tool -y
+pkg install unrar -y
+pkg install gcc -y
+pkg install gdb -y
+pkg install golang -y
+pkg install sqlmap -y
+pip install wafw00f
+pip install sqlmap
+pkg install arp-scan -y
+pkg install hping3 -y
+pkg install strings
+pkg install ltrace -y
+pkg install strace -y
+pkg install objdump -y
+
+
